a402acc4836088e77880f6003506955125bf2a21
Natasha De Mello <nathmello29@gmail.com>
2025-08-14 22:05:24 -0300

    Desenvolvimento da Interface Principal
    Criada a interface principal com o logotipo, o titulo, os botões e a lista com as compras.

--- ListaDeCompras/Form1.cs
+++ ListaDeCompras/Form1.cs
@@ -16,5 +16,10 @@ namespace ListaDeCompras
         {
             InitializeComponent();
         }
+
+        private void lblTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
